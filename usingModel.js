a2d4aa23cdccd750e87d69923de187e287633cf2
Dipak9823 <patildipak363@gmial.com>
2020-04-28 17:39:27 +0530

    using package from npm

---
+++ usingModel.js
@@ -0,0 +1,17 @@
+const MathematicalOperation = require('mathematical-operations');
+
+obj = new MathematicalOperation(16,4);
+
+console.log('sum is = ',obj.sum());
+ 
+ console.log('substraction is = ',obj.substract());
+
+ console.log('multiplication is = ',obj.multiply());
+ 
+ console.log('division is = ',obj.division());
+ 
+ console.log('square is = ',obj.square());
+ 
+ console.log('square root is = ',obj.squareRoot());
+ 
+ 
